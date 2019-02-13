7328d52f5372d6f12a99005dccae1c66c0e25231
Hamhire Hu <me@huhamhire.com>
2019-02-13 16:26:21 +0800

    support yaml config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,19 @@
+import _ from 'lodash';
 import os from 'os';
+import fs from 'fs';
+import nconf from 'nconf';
+import { safeLoad, safeDump } from 'js-yaml';
+
 import { DatabaseConfig } from './config.database';
 import { ServerConfig } from './config.server';
 import { ServiceConfig } from './config.service';
 import { SystemConfig } from './config.system';
+import { pkg } from './pkg';
 
 enum RunEnv {
-    local = 'local',
+    dev = 'develop',
     test = 'test',
-    production = 'production'
+    prod = 'production'
 }
 
 interface ConfigInterface {
@@ -17,41 +23,59 @@ interface ConfigInterface {
     readonly service: ServiceConfig;
 }
 
-const pkg = require('../../package.json');
+const configFormat: Record<string, nconf.IFormat> = {
+    yaml: {
+        parse: (str: string) => safeLoad(str),
+        stringify: (obj: object) => safeDump(obj)
+    },
+    json: nconf.formats.json
+};
 
 class Config implements ConfigInterface {
-    private static _instance = new Config();
-    private readonly _config: ConfigInterface;
-
-    constructor () {
-        const config = require('../../config/config.default.json');
-        this._config = config;
-    }
-
     public readonly hostname = os.hostname();
     public readonly version = pkg.version;
-    // TODO
-    public readonly env = '';
+    public readonly env: RunEnv;
+    public readonly homePath: string;
+
+    constructor () {
+        nconf.use('memory');
+        nconf.env();
+        this.env = RunEnv[nconf.get('NODE_ENV') as keyof typeof RunEnv] || RunEnv.dev;
+        this.homePath = nconf.any(['USERPROFILE', 'HOME']);
+        // load config by environment
+        this._loadConf(`./conf.d/config.${ this.env }`, this.env);
+        // load default config
+        this._loadConf('./conf.d/config.default', 'default');
+    }
+
+    private _loadConf(path: string, env: string): void {
+        _.forEach(configFormat, (format, type) => {
+            const file = `${ path }.${ type }`;
+            if (!fs.existsSync(file)) {
+                return;
+            }
+            console.log(`Loading ${ env } config: "${ file }"`);
+            nconf.file(env, { file, format });
+            // only load same env for one format
+            return false;
+        });
+    }
 
     public get server() {
-        return this._config.server;
+        return nconf.get('server');
     }
 
     public get database() {
-        return this._config.database;
+        return nconf.get('database');
     }
 
     public get system() {
-        return this._config.system;
+        return nconf.get('system');
     }
 
     public get service() {
-        return this._config.service;
-    }
-
-    public static getInstance(): Config {
-        return Config._instance;
+        return nconf.get('service');
     }
 }
 
-export const config = Config.getInstance();
+export const config = new Config();
