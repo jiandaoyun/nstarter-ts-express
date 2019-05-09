22b19ab538f268bd65919a2b1574239e43b2dcc9
Hamhire Hu <me@huhamhire.com>
2019-05-09 16:43:43 +0800

    adjust error & router methods

--- template/src/routes/demo.router.ts
+++ template/src/routes/demo.router.ts
@@ -1,23 +1,21 @@
 import { RequestHandler } from 'express';
 import { Errors } from '../errors';
 
-export class DemoRouter {
-    public static goWelcomeView: RequestHandler = (req, res, next) => {
-        return res.render('welcome', {
-            //#module i18n
-            //#alt
-            //# title: 'To Infinity and Beyond!'
-            //#endalt
-            title: req.i18n.t('page.demo.title')
-            //#endmodule i18n
-        });
-    }
+export const goWelcomeView: RequestHandler = (req, res, next) => {
+    return res.render('welcome', {
+        //#module i18n
+        //#alt
+        //# title: 'To Infinity and Beyond!'
+        //#endalt
+        title: req.i18n.t('page.demo.title')
+        //#endmodule i18n
+    });
+};
 
-    public static goErrorView: RequestHandler = (req, res, next) => {
-        return next(Errors.user(1001));
-    }
+export const goErrorView: RequestHandler = (req, res, next) => {
+    return next(Errors.user(1001));
+};
 
-    public static doPing: RequestHandler = (req, res, next) => {
-        return res.json({ 'msg': 'pong' });
-    }
-}
+export const doPing: RequestHandler = (req, res, next) => {
+    return res.json({ 'msg': 'pong' });
+};
