a2acf16b0f1fa46ad73d6d07689282d2583080ae
Hamhire Hu <me@huhamhire.com>
2019-07-12 11:51:19 +0800

    update: 连接对象报错支持返回连接名称

--- template/src/components/lib/database/mongodb.connection.ts
+++ template/src/components/lib/database/mongodb.connection.ts
@@ -1,13 +1,18 @@
+import _ from 'lodash';
 import mongoose, { Connection } from 'mongoose';
 import { MongodbConfig } from '../../../config/database.config';
 import { logger } from '../logger';
 
 export class MongodbConnector {
     private readonly _options: MongodbConfig;
+    private readonly _name: string = '';
     public readonly connection: Connection;
 
-    constructor (options: MongodbConfig) {
+    constructor (options: MongodbConfig, name?: string) {
         this._options = options;
+        if (name) {
+            this._name = name;
+        }
         this.connection = mongoose.createConnection(this.mongoUri, {
             autoReconnect: true,
             connectTimeoutMS: 10000,
@@ -19,22 +24,34 @@ export class MongodbConnector {
             useCreateIndex: true,
             useNewUrlParser: true
         });
+        this.connection.on('error', (err) => {
+            logger.error(`${ this._tag } connection failed`, { error: err });
+        });
         this.connection.once('open', () => {
-            this.connection.on('error', (err) => {
-                logger.error('Mongodb connection failed', { error: err });
-            });
             this.connection.on('disconnected', () => {
-                logger.error('Mongodb disconnected');
+                logger.error(`${ this._tag } disconnected`);
                 return process.exit(1);
             });
             this.connection.on('reconnected', () => {
-                logger.error('Mongodb reconnected');
+                logger.error(`${ this._tag } reconnected`);
             });
         });
     }
 
     private get mongoUri(): string {
-        const {user, password, mongod, db} = this._options;
-        return `mongodb://${ user }:${ password }@${ mongod.host }:${ mongod.port }/${ db }`;
+        const { user, password, mongod, mongos, db } = this._options;
+        let server;
+        if (mongos) {
+            server = _.join(_.map(mongos, (server) => `${ server.host }:${ server.port }`), ',');
+        } else if (mongod) {
+            server = `${ mongod.host }:${ mongod.port }`;
+        } else {
+            return '';
+        }
+        return `mongodb://${ user }:${ password }@${ server }/${ db }`;
+    }
+
+    private get _tag(): string {
+        return `Mongodb${ this._name ? `:${ this._name }` : ''}`;
     }
 }
--- template/src/components/lib/database/redis.connection.ts
+++ template/src/components/lib/database/redis.connection.ts
@@ -5,17 +5,25 @@ import { logger } from '../logger';
 
 export class RedisConnector {
     private readonly _options: RedisConfig;
+    private readonly _name: string = '';
     public readonly connection: Redis;
 
-    constructor (options: RedisConfig) {
+    constructor (options: RedisConfig, name?: string) {
         this._options = options;
+        if (name) {
+            this._name = name;
+        }
 
         const o = _.defaults<RedisOptions, RedisConfig>({
             retryStrategy: () => 1000
         }, this._options);
         this.connection = new IORedis(o);
         this.connection.on('error', (err) => {
-            logger.error(err);
+            logger.error(`${ this._tag } connection error`, { error: err });
         });
+    }
+
+    private get _tag(): string {
+        return `Redis${ this._name ? `:${ this._name }` : ''}`;
     }
 }
