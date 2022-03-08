ad68df96798e60d04ba5241d5ad78dbaefca4212
kyle <kyle@finereport.com>
2022-03-08 15:21:18 +0800

    update: FXD-22621 升级 core && 调整 requestId 初始化依赖

--- template/src/context.ts
+++ template/src/context.ts
@@ -1,13 +1,5 @@
-import { Request } from 'express';
 import { BaseContext } from 'nstarter-core';
 
 export class Context extends BaseContext {
 
-    /**
-     * 根据请求初始化上下文
-     * @param req - 请求对象
-     */
-    public setByRequest(req: Request) {
-        super.setByRequest(req);
-    }
 }
